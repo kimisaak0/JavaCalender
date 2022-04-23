ee39ce9e0a7ee166c28ae801445ce86bfeebcc4b
isaak <kimisaak0@gmail.com>
2022-04-23 22:58:36 +0900

    입력받은 달이 몇 일까지 있는지 확인하는 코드

--- src/Calender.java
+++ src/Calender.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Calender {
     public static void main(String[] args) {
+        int[] monthEnd = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        Scanner sc = new Scanner(System.in);
+
+        int end;
+        System.out.println("반복 횟수를 입력하세요");
+        end = sc.nextInt();
+
+        for(int i = 0; i < end; i++) {
+            System.out.println("원하는 달을 입력하세요.");
+            int n;
+            do {
+                n = sc.nextInt();
+                if(n < 1 || n > 12) {
+                    System.out.println("1~12 사이의 값을 입력하세요.");
+                    System.out.println("원하는 달을 입력하세요.");
+                }
+            } while(n < 1 || n > 12);
+
+            System.out.printf("%d월은 %d일까지 있습니다. \n",n,monthEnd[n-1]);
+        }
+
+
         System.out.println("일\t월\t화\t수\t목\t금\t토");
         System.out.println("--------------------------");
         System.out.println("1\t2\t3\t4\t5\t6\t7");
